74e526fbd18a83fc2b524e1d98f8a53c00812578
C107165108 <80106843+C107165108@users.noreply.github.com>
2021-04-15 14:26:33 +0800

    0415 work

---
+++ 0415/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Clock from './Clock';
+import reportWebVitals from './reportWebVitals';
+
+
+
+ReactDOM.render(
+  <Clock/>,
+  document.getElementById('root')
+);
+
+
+
+reportWebVitals();
