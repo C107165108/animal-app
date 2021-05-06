6635edeaf8012af9be270f6018de09e4eb1b33ed
C107165108 <80106843+C107165108@users.noreply.github.com>
2021-05-06 10:44:04 +0800

    0408 arrow function

---
+++ 04.08/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App5';
+import reportWebVitals from './reportWebVitals';
+
+
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
+
+
+
+reportWebVitals();
