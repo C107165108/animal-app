d1ccc8b8cd54edbea2a67f66fe4defce7cf0a581
C107156126 <76277840+C107156126@users.noreply.github.com>
2022-01-05 18:37:44 +0800

    主頁風格調整

--- animal/src/Home.js
+++ animal/src/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 
@@ -18,20 +18,48 @@ export default class Home extends Component {
         Actions.push('HelpStreet');
     }
 
-    
+
     render() {
 
-        const { handleRedirectHelpMap, handleRedirectReportForm, handleRedirectHelpList ,handleRedirectHelpStreet} = this;
+        const { handleRedirectHelpMap, handleRedirectReportForm, handleRedirectHelpList, handleRedirectHelpStreet } = this;
 
 
         return (
-            <View>
-                <Text>Home</Text>
-                <TouchableOpacity onPress={handleRedirectHelpMap}><Text>map</Text></TouchableOpacity>
-                <TouchableOpacity onPress={handleRedirectReportForm}><Text>ReportForm</Text></TouchableOpacity>
-                <TouchableOpacity onPress={handleRedirectHelpList}><Text>HelpList</Text></TouchableOpacity>
-                <TouchableOpacity onPress={handleRedirectHelpStreet}><Text>HelpStreet</Text></TouchableOpacity>
+            <View style={styles.container}>
+                <Text style={styles.title}>Home</Text>
+                <TouchableOpacity style={styles.button} onPress={handleRedirectHelpMap}><Text style={styles.text}>救援地圖</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleRedirectReportForm}><Text style={styles.text}>表單申請</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.button}  onPress={handleRedirectHelpList}><Text style={styles.text}>救援清單</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.button}  onPress={handleRedirectHelpStreet}><Text style={styles.text}>街景狀況</Text></TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+
+    container: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize:20,
+    },
+    title: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize:20,
+    },
+    button: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 200,
+        height: 100,
+        backgroundColor: '#68a0cf',
+        overflow: 'hidden',
+        margin: 10,
+        borderradius:10,
+    },
+});
